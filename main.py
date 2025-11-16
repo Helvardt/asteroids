14005a2c9cef157a3cf27a3da4230c3696cd5584
Fal <129197356+Helvardt@users.noreply.github.com>
2025-11-16 13:38:34 +0100

    log add

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def gameloop():
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+    
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
--- test_pygame.py
+++
@@ -1,5 +0,0 @@
-import sys
-print("Python executable:", sys.executable)
-
-import pygame
-print("Pygame version:", pygame.__version__)
