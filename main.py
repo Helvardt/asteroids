212f144fb8d0e9a10ee3e1a340ab25b2cd2c5434
Fal <129197356+Helvardt@users.noreply.github.com>
2025-11-16 13:54:23 +0100

    big add

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()  # create an instance
 
 def gameloop():
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,12 +16,13 @@ def gameloop():
                 return
         screen.fill("black")
         pygame.display.flip()
-    
+        dt = clock.tick(60) / 1000  # use the instance
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+    gameloop()  # start the loop
+
 if __name__ == "__main__":
     main()
